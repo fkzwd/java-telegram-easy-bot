daba2dbe40b34c95ae178fd0b67ad3d4a98714b7
fkzwd <lives352@gmail.com>
2024-12-16 23:29:33 +0000

    upd readme; refactor chained input wait

--- example-echo-bot/src/main/java/com/vk/dwzkf/tglib/example/echobot/commands/ChainedInputWait.java
+++ example-echo-bot/src/main/java/com/vk/dwzkf/tglib/example/echobot/commands/ChainedInputWait.java
@@ -2,22 +2,18 @@ package com.vk.dwzkf.tglib.example.echobot.commands;
 
 import com.vk.dwzkf.tglib.botcore.annotations.RouteCommand;
 import com.vk.dwzkf.tglib.botcore.annotations.RouteCommandHandler;
-import com.vk.dwzkf.tglib.botcore.context.MessageContext;
-import com.vk.dwzkf.tglib.botcore.enums.InputFlag;
 import com.vk.dwzkf.tglib.botcore.exception.BotCoreException;
 import com.vk.dwzkf.tglib.botcore.exception.OnInputException;
 import com.vk.dwzkf.tglib.botcore.forms.Form;
 import com.vk.dwzkf.tglib.botcore.forms.actions.ActionType;
-import com.vk.dwzkf.tglib.botcore.forms.actions.AfterClickAction;
 import com.vk.dwzkf.tglib.botcore.input.InputAwait;
 import com.vk.dwzkf.tglib.botcore.input.InputWaiterService;
 import com.vk.dwzkf.tglib.example.echobot.context.CustomContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.objects.Message;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author Roman Shageev
@@ -29,59 +25,63 @@ import java.util.List;
 public class ChainedInputWait {
     private final InputWaiterService inputWaiterService;
 
-    private static class ChainedInputContext {
-        private String firstMame;
-        private String lastName;
-        private int age;
+    @RouteCommandHandler
+    public void handle(CustomContext ctx) throws BotCoreException {
+        ChainedInputContext context = new ChainedInputContext();
+        ctx.doReply("Введите имя");
+        inputWaiterService
+                .await(
+                        getSimpleString(
+                                value -> context.firstName = value,
+                                "Некорректный ввод. Введите имя",
+                                "Введите фамилию"
+                        ),
+                        ctx
+                )
+                .thenAwait(getSimpleString(
+                        value -> context.lastName = value,
+                        "Некорректный ввод. Введите фамилию",
+                        "Введите возраст"
+                ))
+                .thenAwait(getAge(context));
     }
 
-    @RouteCommandHandler
-    public void handle(CustomContext ctx) throws BotCoreException  {
-        ChainedInputContext context = new ChainedInputContext();
-        ctx.doReply("Введите имя").getMessageId();
-        inputWaiterService
-                .await(inputContext -> {
-                            String input = inputContext.getMessage();
-                            if (!input.matches("[a-zA-Z]+")) {
-                                throw new OnInputException(
-                                        List.of(
-                                                ActionType.REPLY_TEXT.createAction("Некорректный ввод. Введите имя")
-                                        )
-                                ).setInterrupt(false);
-                            }
-                            context.firstMame = input;
-                            return List.of(
-                                    ActionType.REPLY_TEXT.createAction("Введите Фамилию")
-                            );
-                        }, ctx
-                )
-                .thenAwait(inputContext -> {
-                    String message = inputContext.getMessage();
-                    if (!message.matches("[a-zA-Z]+")) {
-                        throw new OnInputException(
-                                List.of(
-                                        ActionType.REPLY_TEXT.createAction("Некорректный ввод. Введите фамилию")
-                                )
-                        ).setInterrupt(false);
-                    }
-                    context.lastName = message;
-                    return List.of(
-                            ActionType.REPLY_TEXT.createAction("Введите возраст")
-                    );
-                }).thenAwait(inputContext -> {
-                    String message = inputContext.getMessage();
-                    if (!message.matches("\\d+")) {
-                        throw new OnInputException(
-                                List.of(
-                                        ActionType.REPLY_TEXT.createAction("Некорректный ввод. Введите возраст")
-                                )
-                        ).setInterrupt(false);
-                    }
-                    context.age = Integer.parseInt(message);
-                    return List.of(
-                            ActionType.CREATE_FORM.createAction(createDataForm(context))
-                    );
-                });
+    private InputAwait getAge(ChainedInputContext context) throws OnInputException {
+        return inputContext -> {
+            String message = inputContext.getMessage();
+            if (!message.matches("\\d+")) {
+                throw new OnInputException(
+                        List.of(
+                                ActionType.REPLY_TEXT.createAction("Некорректный ввод. Введите возраст")
+                        )
+                ).setInterrupt(false);
+            }
+            context.age = Integer.parseInt(message);
+            return List.of(
+                    ActionType.CREATE_FORM.createAction(createDataForm(context))
+            );
+        };
+    }
+
+    private static InputAwait getSimpleString(
+            Consumer<String> valueConsumer,
+            String errorMessage,
+            String replyMessage
+    ) throws OnInputException {
+        return inputContext -> {
+            String message = inputContext.getMessage();
+            if (!message.matches("[a-zA-Z]+")) {
+                throw new OnInputException(
+                        List.of(
+                                ActionType.REPLY_TEXT.createAction(errorMessage)
+                        )
+                ).setInterrupt(false);
+            }
+            valueConsumer.accept(message);
+            return List.of(
+                    ActionType.REPLY_TEXT.createAction(replyMessage)
+            );
+        };
     }
 
     private Form createDataForm(ChainedInputContext ctx) {
@@ -92,12 +92,18 @@ public class ChainedInputWait {
                     <b>Фамилия:</b> %s
                     <b>Возраст:</b> %s
                     """.formatted(
-                    ctx.firstMame,
+                    ctx.firstName,
                     ctx.lastName,
                     ctx.age
             );
         });
         form.createControls(false, true);
         return form;
+    }
+
+    private static class ChainedInputContext {
+        private String firstName;
+        private String lastName;
+        private int age;
     }
 }
